0e382dcf1da69e8038be2d41f5f3de6e063de7fc
ls <izqiaulhassan166@gmail.com>
2026-07-21 18:12:28 +0500

    useActionstate

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,39 +1,20 @@
 'use client'
-import { useState } from 'react'
+import { useActionState, useState } from 'react'
+import {sayhello , Actiontype} from "./actions/actions"
 
-
+const initailstate :Actiontype = {message : ""}
 const page = () => {
-  const [username , setUsername] = useState("")
-  const [result ,setResult] = useState<any>(null)
-
-  const handlesubmit = async(e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault();
-    const res = await fetch('/api/data' , {
-      method : "POST",
-      headers : {"Content-Type" : "application/json"},
-      body :JSON.stringify({username})
-    })
-    const data = await res.json()
-    setResult(data)
-  }
-
-  const fetchall = async ()=>{
-    const res = await fetch ("/api/data" , {method:"GET"})
-    const data = await res.json()
-    setResult(data)
-  }
+  const [ state , formAction ] = useActionState(sayhello , initailstate)
 
   return (
     <div>
-      <form onSubmit={handlesubmit}>
-        <input type="text" placeholder='Enter name' value={username} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setUsername(e.target.value)} />
+      <form action={formAction}>
+        <input type="text" placeholder='Enter name' name='name'  />
 
         <button type='submit'>submit</button>
+        <p>{state.message}</p>
       </form>
-      <button onClick={fetchall}>Fetch all</button>
-      {result &&(
-        <pre>{JSON.stringify(result , null , 2)}</pre>
-      )}
+      
     </div>
   )
 }
